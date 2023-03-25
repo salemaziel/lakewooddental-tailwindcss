f29622580074e0a67d079403df4974c6572669c2
salemaziel <mymainemail0501@gmail.com>
2023-03-24 19:27:10 -0700

    fasf

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout/layout";
 import Seo from "../components/seo";
 import Home from "../components/pageComponents/Home";
 
+import OGImage from "../images/favion.png"
+
 const IndexPage = () => (
     <Layout>
         <Home />
@@ -18,5 +20,9 @@ const IndexPage = () => (
 export default IndexPage;
 
 export const Head = () => (
-<Seo title="Home" />
+<Seo 
+title="Lakewood Dental | Serving Lakewood, CA area for over 30 years."
+description="Lakewood Dental has been serving the Lakewood, CA area for over 30 years."
+ogImage={OGImage}
+ />
 )
